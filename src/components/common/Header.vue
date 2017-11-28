1f5a4f586f6392c71a5a0a7f88d8c06947c20303
moveondo <1020124712@qq.com>
2017-11-28 09:53:20 +0800

    Update Header.vue

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="header">
-        <div class="logo">信用精灵后台管理系统</div>
+        <div class="logo">后台管理系统</div>
         <div class="user-info">
             <el-dropdown trigger="click" @command="handleCommand">
                 <span class="el-dropdown-link">
